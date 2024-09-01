6cce6591269e2a9bddcd036e82ea44b4679c7072
King Matayo <kingmatayo.thefirst@gmail.com>
2024-09-01 17:32:42 +0300

    Upto data to ink ratio slides 17/32

--- wk2_viz.R
+++ wk2_viz.R
@@ -23,12 +23,46 @@ knitr::opts_chunk$set(
   dpi = 300             # higher dpi, sharper image
 )
 
-#Visualizing duke forest housing prices vs areas dataset
+#Visualizing duke forest dataset: housing prices vs area 
 ggplot(duke_forest, aes(x = area, y = price)) +
   geom_point(alpha = 0.7, size = 2) +
-  geom_smooth(method = "lm", se = FALSE, size = 0.7) +
+  geom_smooth(method = "lm", se = FALSE, linewidth = 0.7) +
   labs(
     x = "Area (square feet)",
     y = "Sale price (USD)",
     title = "Price and area of houses in Duke Forest"
   )
+
+#Mutate the duke_forest dataset to add additional columns
+duke_forest <- duke_forest
+
+duke_forest <- duke_forest %>%
+  mutate(decade_built = (year_built %/% 10) * 10)
+
+duke_forest <- duke_forest |> 
+  mutate(
+    decade_built_cat = case_when(
+      decade_built <= 1940 ~ "1940 or before",
+      decade_built >= 1990 ~ "1990 or after",
+      TRUE ~ as.character(decade_built)
+    )
+  )
+
+#Slightly more complex visualization
+ggplot(
+  duke_forest,
+  aes(x = area, y = price, color = decade_built_cat)
+) +
+  geom_point(alpha = 0.7, show.legend = FALSE) +
+  geom_smooth(method = "lm", se = FALSE, linewidth = 0.5, show.legend = FALSE) +
+  facet_wrap(~decade_built_cat) +
+  labs(
+    x = "Area (square feet)",
+    y = "Sale price (USD)",
+    color = "Decade built",
+    title = "Price and area of houses in Duke Forest"
+  )
+
+# Data to ink ratio - continue from here next time
+
+
